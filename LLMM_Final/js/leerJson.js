c06622e371f3d5feb4993fed80b6c8b7631db969
ivanrmn <ivanrmnh@gmail.com>
2018-06-02 02:31:40 +0200

    Segunda prueba lectura json

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -1,12 +1,40 @@
-$.getJSON('../json/articulo1.json', function(data) {
-  var items = [];
- 
-  $.each(data, function(key, val) {
-    items.push('<h3 id="' + key + '">' + val + '</h3');
-  });
- 
-  $('</p>', {
-    'class': 'leerJson1',
-    html: items.join('')
-  }).appendTo('pruebajson');
-});
+articulo1.titulo
+articulo1.descripcion
+
+articulo1['titulo'][1]['descripcion'][2]
+
+
+var requestURL = './json/articulo1';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+	var articulo1 = request.response;
+	populateHeader(articulo1);
+	mostrarJson(articulo1);
+}
+
+function populateHeader(jsonObj) {
+	var myH3 = document.createElement('h3');
+	myH3.textContent = jsonObj['titulo'];
+	header.appendChild(myH3);
+	
+	var myPara = document.createElement('p');
+	myPara.textContent = 'descripcion: ' + jsonObj['descripcion'] + ' // Formed: ' + jsonObj['formed'];
+	header.appendChild(myPara);
+}
+
+function mostrarNoticias(jsonObj) {
+    var myArticle = document.createElement('article');
+    var myH3 = document.createElement('h3');
+    var myPara1 = document.createElement('p');
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+
+    section.appendChild(myArticle);
+  }
+}
