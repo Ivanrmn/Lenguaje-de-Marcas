31723756c8c01462842cf1f3fa30bf51c6457d6c
ivanrmn <ivanrmnh@gmail.com>
2018-06-02 03:21:16 +0200

    Tercera prueba lectura json

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -1,40 +1,21 @@
-articulo1.titulo
-articulo1.descripcion
+$(function(){
+    //cargar noticias
+    $.getJSON( "./json/articulo1.json", function( jsonObject ) {
+        ponerNoticias( jsonObject );
+    });
 
-articulo1['titulo'][1]['descripcion'][2]
+    //onclick en verinfo
+    $("#verinfo").click(function() {
+        var v=$("#noticias").val();
+        if (v!=0) window.location = "/articulo1/"+$("#noticias").val();
+    });
+});
 
-
-var requestURL = './json/articulo1';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-	var articulo1 = request.response;
-	populateHeader(articulo1);
-	mostrarJson(articulo1);
-}
-
-function populateHeader(jsonObj) {
-	var myH3 = document.createElement('h3');
-	myH3.textContent = jsonObj['titulo'];
-	header.appendChild(myH3);
-	
-	var myPara = document.createElement('p');
-	myPara.textContent = 'descripcion: ' + jsonObj['descripcion'] + ' // Formed: ' + jsonObj['formed'];
-	header.appendChild(myPara);
-}
-
-function mostrarNoticias(jsonObj) {
-    var myArticle = document.createElement('article');
-    var myH3 = document.createElement('h3');
-    var myPara1 = document.createElement('p');
-
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-
-    section.appendChild(myArticle);
-  }
+function ponerNoticias(json){
+     $.each( json, function( i, articulo1 ) {
+         //dos formas de hacer lo mismo, la segunda es más adecuada.
+         $("#noticias").append( "<h3 value='" + articulo1.titulo + "'>" + "</h3>" + "<p>" + articulo1.descripcion + "</p>" );
+         //$('#noticias').append($('<option>', { value: noticia.slug, text : noticia.comunidad }));
+         //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
+     }); 
 }
