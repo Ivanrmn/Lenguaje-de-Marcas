2c1bcdde36353b4700ab6ee4bc7b9458afede333
ivanrmn <ivanrmnh@gmail.com>
2018-06-03 22:41:21 +0200

    json

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -7,11 +7,14 @@ $(document).ready(function(){
 
         function imprimirJson(json){
          $.each( json, function( i, noticia1 ) {
-          $("#cargarJson").append( "<h3 id='" + i + "'>" + noticia1.title + "</h3>" + "<img class='img-fluid float-left img-thumbnail mr-4' src='" + noticia1.img + "'>" + "<p>" + noticia1.description + "</p>" + "<div class='form-control' <input class='form-control' type='datetime-local'>" + noticia1.datetime + "'>" + "</div>");
+          $("#cargarJson").append( "<h3 id='" + i + "'>" + noticia1.title + "</h3>" + 
+		  "<img class='rounded' src='" + noticia1.img + "'>" + "<p>" + noticia1.description + "</p>" + 
+		  "<div class='form-control' <input class='form-control' type='datetime-local' value='" + noticia1.datetime + "'>" + "</div>");
          }); 
         }
    });
 });
+
 
 $("#noticia1").click(function () {
     window.location.href = "../html/news1.html"
