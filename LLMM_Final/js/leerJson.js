db13888198c896d3ac4cd54907adc553eaa9374d
ivanrmn <ivanrmnh@gmail.com>
2018-06-03 15:58:13 +0200

    Prueba json

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -1,4 +1,4 @@
-$(function(){
+function leerArticulo1() {
     //cargar noticias
     $.getJSON( "../json/articulo1.json", function( jsonObject ) {
 		console.log(jsonObject);
@@ -8,6 +8,7 @@ $(function(){
 });
 
 function ponerNoticias(json){
+	leerArticulo1();
      $.each( json, function( i, articulo1 ) {
          $("#leerJson").append( "<h3 id='" + i + "'>" + articulo1.title + "</h3>" + "<p>" + articulo1.description + "</p>" );
      }); 
