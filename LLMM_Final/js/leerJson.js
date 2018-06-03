c3d6befcac1da166b85aeffcaa7ff184ee55ede1
ivanrmn <ivanrmnh@gmail.com>
2018-06-03 22:15:41 +0200

    leerJson

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -1,4 +1,4 @@
-/*$(document).ready(function(){ 
+$(document).ready(function(){ 
    $('#leerJson').on('click',function(){
         $.getJSON( "../json/articulo1.json", function( jsonObject ) {
 			console.log(jsonObject);
@@ -7,22 +7,10 @@
 
         function imprimirJson(json){
          $.each( json, function( i, noticia1 ) {
-          $("#cargarJson").append( "<h3 id='" + i + "'>" + noticia1.title + "</h3>" + "<p>" + "<img class='img-fluid float-left img-thumbnail mr-4' src='" + noticia1.img + "'>" + noticia1.description + "</p>" + "<div class='form-control'>" + noticia1.datetime + "</div>");
+          $("#cargarJson").append( "<h3 id='" + i + "'>" + noticia1.title + "</h3>" + "<img class='img-fluid float-left img-thumbnail mr-4' src='" + noticia1.img + "'>" + "<p>" + noticia1.description + "</p>" + "<div class='form-control' <input class='form-control' type='datetime-local'>" + noticia1.datetime + "'>" + "</div>");
          }); 
         }
    });
-});*/
-
-$.getJSON( "../json/articulo1.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
 });
 
 $("#noticia1").click(function () {
