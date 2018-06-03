c67453120d98776c3f27462a5a6c23de42f16bd3
ivanrmn <ivanrmnh@gmail.com>
2018-06-03 16:12:41 +0200

    Prueba leerJson

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -1,15 +1,8 @@
 function leerArticulo1() {
-    //cargar noticias
     $.getJSON( "../json/articulo1.json", function( jsonObject ) {
-		console.log(jsonObject);
-        ponerNoticias( jsonObject );
+		$.each( json, function( i, articulo1 ) {
+			$("#leerJson").append( "<h3 id='" + i + "'>" + articulo1.title + "</h3>" + "<p>" + articulo1.description + "</p>" );
+
     });
 
 });
-
-function ponerNoticias(json){
-	leerArticulo1();
-     $.each( json, function( i, articulo1 ) {
-         $("#leerJson").append( "<h3 id='" + i + "'>" + articulo1.title + "</h3>" + "<p>" + articulo1.description + "</p>" );
-     }); 
-}
