ee8a17cbe9211f866b34918a13d21e22655eb2d7
ivanrmn <ivanrmnh@gmail.com>
2018-06-03 16:33:34 +0200

    json

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -1,8 +1,14 @@
 function leerArticulo1() {
     $.getJSON( "../json/articulo1.json", function( jsonObject ) {
 		$.each( json, function( i, articulo1 ) {
+			console.log(jsonObject);
 			$("#leerJson").append( "<h3 id='" + i + "'>" + articulo1.title + "</h3>" + "<p>" + articulo1.description + "</p>" );
 
     });
-
 });
+
+};
+
+function mostrarJson() {
+	leerArticulo1();
+}
