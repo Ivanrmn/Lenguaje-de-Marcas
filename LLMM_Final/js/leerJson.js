90a0d040b99bff173d21d1c20689569a723c74d2
ivanrmn <ivanrmnh@gmail.com>
2018-06-04 16:35:05 +0200

    json

--- LLMM_Final/js/leerJson.js
+++ LLMM_Final/js/leerJson.js
@@ -1,43 +1,40 @@
-/*var contador = 2;
+var contador = 2;
+let i = 1;
 
-function load() {
-	for (i = 1; i <= contador; i++) {
-		$('#leerJson').on('click',function(){
-			$.getJSON("https://rawgit.com/Ivanrmn/Lenguaje-de-Marcas/master/LLMM_Final/json/articulo" + i + ".json", function (jsonObject) {
-				imprimirJson(jsonObject);
-		});
-		});
-	$('#leerJson').hide();
-};
-};
-
-function imprimirJson(json){
-    $.each( json, function( i, noticia ) {
-        $("#cargarJson").append( "<div class='Box-noticia'>" + "<h3 id='" + i + "'>" + noticia.title + "</h3>" + 
-		"<img class='rounded' src='" + noticia.img + "'>" + "<p>" + noticia1.description + "</p>" + 
-		"<div class='form-control' <input class='form-control' type='datetime-local' value='" + noticia.datetime + "'>" + "</div>" + "</div>");
-         }); 
-        };*/
-
-		
-$(document).ready(function(){ 
-   $('#leerJson').on('click',function(){
-        $.getJSON( "https://rawgit.com/Ivanrmn/Lenguaje-de-Marcas/master/LLMM_Final/json/articulo1.json", function( jsonObject ) {
-			console.log(jsonObject);
-            imprimirJson( jsonObject );
-			$('#leerJson').hide();
-        });
-
-        function imprimirJson(json){
-         $.each( json, function( i, noticia ) {
-          $("#cargarJson").append( "<div class='Box-noticia'>" + "<h3 id='" + i + "'>" + noticia.title + "</h3>" + 
-		  "<img class='rounded' src='" + noticia.img + "'>" + "<p>" + noticia.description + "</p>" + 
-		  "<div class='form-control' <input class='form-control' type='datetime-local' value='" + noticia.datetime + "'>" + "</div>" + "</div>");
-         }); 
+$(document).ready(function () {
+    $('#leerJson').on('click', function () {
+        switch (i) {
+            case 1:
+                $.getJSON("https://rawgit.com/Ivanrmn/Lenguaje-de-Marcas/master/LLMM_Final/json/articulo1.json", function (jsonObject) {
+                    console.log(jsonObject)
+                    imprimirJson(jsonObject);
+                    i++;
+                    console.log(i)
+                });
+                break;
+            case 2:
+                $.getJSON("https://rawgit.com/Ivanrmn/Lenguaje-de-Marcas/master/LLMM_Final/json/articulo2.json", function (jsonObject) {
+                    console.log(jsonObject)
+                    imprimirJson(jsonObject);
+                    i++
+                });
+                break;
+            default:
+                $("#leerJson").hide();
         }
-   });
+    });
+
+    function imprimirJson(json) {
+        $.each(json, function (i, noticia) {
+            $("#cargarJson").append("<div class='Box-noticia'>" + "<h3 id='" + i + "'>" + noticia.title + "</h3>" +
+                "<img class='rounded' src='" + noticia.img + "'>" + "<p>" + noticia.description + "</p>" +
+                "<div class='form-control' <input class='form-control' type='datetime-local' value='" + noticia.datetime + "'>" + "</div>" + "</div>");
+        });
+    }
 });
-		
+
+
+
 /*$("#noticia1").click(function() {
     window.location.href = "https://rawgit.com/Ivanrmn/Lenguaje-de-Marcas/master/LLMM_Final/html/news1.html"
 });
